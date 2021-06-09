bcf17629caf75cdaa34803a96b3c73fc207ff3ce
Diego Prestes <diegoprestes.git@hotmail.com>
2021-06-09 20:30:41 -0300

    feat: add verification for invalid email

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -2,8 +2,16 @@ import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../erros/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../erros/invalid-param-error copy'
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
@@ -11,10 +19,10 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
-
-    return {
-      statusCode: 200,
-      body: new MissingParamError('xesque')
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
+    return badRequest(new MissingParamError('temp'))
   }
 }
