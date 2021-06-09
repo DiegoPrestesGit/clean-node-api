4adeb9d5dfdfeee4c54336c724f4f3a412fbd55c
Diego Prestes <diegoprestes.git@hotmail.com>
2021-06-09 20:41:15 -0300

    style: change name of the file invalid-param-error

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -3,7 +3,7 @@ import { MissingParamError } from '../erros/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
-import { InvalidParamError } from '../erros/invalid-param-error copy'
+import { InvalidParamError } from '../erros/invalid-param-error'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
